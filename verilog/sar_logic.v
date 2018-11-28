08c4b60bc56083ba64b1c6b1b0d2fdca4827eebe
minyun168 <minyun168@outlook.com>
2018-11-29 00:51:12 +0800

    Update sar_logic.v

--- verilog/sar_logic.v
+++ verilog/sar_logic.v
@@ -5,21 +5,25 @@ output[5:0] q,dout;
 output[2:0] count;
 
 reg [5:0] q;
-reg [2:0] dout;
+reg [5:0] dout;
 reg [2:0] count;
 
-parameter c0=3'b000,c1=3'b001,c2=3'b010,c3=3'b011,c4=3'b100,c5=3'b101,c6=3'b110,c7=3'b111;
+parameter c0=3'b000,c1=3'b001,c2=3'b010,c3=3'b011,c4=3'b100,c5=3'b101,c6=3'b110;
 
 always@(posedge clk)
 	if(rest)
-	begin
+		begin
+	
 		dout<=6'b000000;
 		count<=3'b000;
-	end
+	
+		end
+	
 	else
 	case(count)
 		c0:
-		begin
+			begin
+
 			count=3'b000;
 			dout=6'b100000;
 			if(ena)
@@ -32,7 +36,8 @@ always@(posedge clk)
 					count=c0;
 				end
 			q=dout;
-		end
+			
+			end
 	
 		c1:
 			begin
@@ -50,9 +55,9 @@ always@(posedge clk)
 		c2:
 			begin
 
-			dout[5]<=comp;
-			dout[4]<=1;
-			dout[3]<=0;
+			dout[5]<=dout[5];
+			dout[4]<=comp;
+			dout[3]<=1;
 			dout[2]<=0;
 			dout[1]<=0;
 			dout[0]<=0;
@@ -65,9 +70,9 @@ always@(posedge clk)
 			begin
 
 			dout[5]<=dout[5];
-			dout[4]<=comp;
-			dout[3]<=1;
-			dout[2]<=0;
+			dout[4]<=dout[4];
+			dout[3]<=comp;
+			dout[2]<=1;
 			dout[1]<=0;
 			dout[0]<=0;
 
@@ -78,7 +83,57 @@ always@(posedge clk)
 		c4:
 			begin
 
-			dout[5]
+			dout[5]<=dout[5];
+			dout[4]<=dout[4];
+			dout[3]<=dout[3];
+			dout[2]<=comp;
+			dout[1]<=1;
+			dout[0]<=0;
+
+			count<=c5;
+
 			end
+
+		c5:
+			begin
+
+			dout[5]<=dout[5];
+			dout[4]<=dout[4];
+			dout[3]<=dout[3];
+			dout[2]<=dout[2];
+			dout[1]<=comp;
+			dout[0]<=1;
+
+			count<=c6;
+
+			end
+
+		c6:
+			begin
+
+			dout[5]<=dout[5];
+			dout[4]<=dout[4];
+			dout[3]<=dout[3];
+			dout[2]<=dout[2];
+			dout[1]<=dout[1];
+			dout[0]<=comp;
+
+			count<=cout+1;
+
+			end
+
+		default:
+			begin
+
+			count<=3'b0;
+			q<=dout;
+
+			end
+
+	endcase
+
+endmodule
+
+
 
 		
