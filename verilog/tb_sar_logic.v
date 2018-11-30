13b45b819097239168d48a00dc2186d0bc5bdc10
minyun168 <minyun168@outlook.com>
2018-11-30 13:58:31 +0800

    Update tb_sar_logic.v

--- verilog/tb_sar_logic.v
+++ verilog/tb_sar_logic.v
@@ -4,12 +4,26 @@
 module tb_sar_logic;
 
 reg rest,clk,comp,ena;
-wire []
+wire [5:0] q,dout;
+wire [2:0] count;
 
+always #50 clk=~clk;
+always #150 comp=~comp;
+always #3500 ena=~ena;
 
 initial
 	begin
 
-	
+	clk=1;
+	comp=1;
+	ena=0;
+	rest=1;
 
+	#100 rest=0;
+	#1000 $stop;
+	
 	end
+
+sar_logic m(.rest),.clk(clk),.comp)comp),.q(q),.ena(ena),.dout(dout),.count(count);
+
+endmodule
